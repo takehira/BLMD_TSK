9dab870277713d4ce16f0f8b388d2d6c3874b805
blmd-niz <daisuke_niizumi@balmuda.com>
2017-07-20 13:08:09 +0900

    Fix 実行時のSegmentation fault: 11を解決

--- OOP_on_C/main.c
+++ OOP_on_C/main.c
@@ -12,12 +12,18 @@
 lAlgo Factory(int algo_type);
 int Algo_calc(lAlgo algo, int a, int b);
 
+lAlgo test1 = {Algo1_pre_calc, Algo1_post_calc};
 
 int main(void){
 	int res = 0;
 
 	lAlgo algo1 = Factory(ALGO1_TYPE);
 	lAlgo algo2 = Factory(ALGO2_TYPE);
+
+	printf("@main.algo1: 0x%x 0x%x\n", algo1.pre_calc, algo1.post_calc);
+	printf("@main.test1: 0x%x 0x%x\n", test1.pre_calc, test1.post_calc);
+	printf("@main.test1.pre_calc(100, 200): %d\n", test1.pre_calc(100, 200));
+	printf("@main.test1.post_calc(100, 200): %d\n", test1.post_calc(100, 200));
 
 	res = Algo_calc(algo1, 10 ,20);
 	printf("algo1: %d\n", res);
@@ -32,12 +38,15 @@ int main(void){
 lAlgo Factory(int algo_type){
 	lAlgo algo;
 	if(algo_type == ALGO1_TYPE){
-		lAlgo algo = new_Algo1();
+		lAlgo algo1 = new_Algo1();
+		algo = algo1;
 	}else if(algo_type == ALGO2_TYPE){
-		lAlgo algo = new_Algo2();
+		lAlgo algo2 = new_Algo2();
+		algo = algo2;
 	}else{
 	
 	}
+	printf("@Factory algo: 0x%x 0x%x\n", algo.pre_calc, algo.post_calc);
 	return algo;
 }
 
